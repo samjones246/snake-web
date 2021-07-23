5b60e51a0bcb5986ffc48ece251c72044ebdbb31
Sam Jones <sam.jones240699@gmail.com>
2021-07-23 16:39:05 +0100

    added endpoints for retrieving scores

--- back/scores/urls.py
+++ back/scores/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='indexs')
+    path('', views.top_n, name='index'),
+    path('<int:year>', views.top_n, name="scores for year"),
+    path('<int:year>/<int:month>', views.top_n, name="scores for month"),
+    path('<int:year>/<int:month>/<int:day>', views.top_n, name="scores for day"),
+    path('<str:name>', views.scores_name, name="scores for name"),
 ]
--- back/scores/views.py
+++ back/scores/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from .models import HighScore
+from django.core import serializers
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
     return HttpResponse("Hello, World!")
+
+def scores_name(request, name):
+    data = HighScore.objects.filter(name=name)
+    return HttpResponse(serializers.serialize("json", data))
+
+def top_n(request : HttpRequest, year=None, month=None, day=None):
+    data = HighScore.objects.all()
+    if year:
+        data = data.filter(timestamp__year=year)
+    if month:
+        data = data.filter(timestamp__month=month)
+    if day:
+        data = data.filter(timestamp__day=day)
+    data = data.order_by("-score")
+    startn = int(request.GET.get("startn", default=0))
+    n = int(request.GET.get("n", default=-1))
+    data = data[startn:(startn + n if n >= 0 else None)]
+    if not request.GET.get("human"):
+        data = serializers.serialize("json", data)
+    return HttpResponse(data)
