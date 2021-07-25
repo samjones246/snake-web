6253c3b637271cc16d7e639bcdb2fca735d09d57
Sam Jones <sam.jones240699@gmail.com>
2021-07-26 00:38:41 +0100

    trying to pull user high score properly

--- back/back/settings.py
+++ back/back/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
--- front/scripts/scores.js
+++ front/scripts/scores.js
@@ -1,8 +1,12 @@
 const tabs = document.querySelectorAll(".panel_tab")
 const submitBox = document.getElementById("score_submit")
+const nameEntry = document.getElementById("submit_name")
 const leaderboardRows = document.querySelectorAll("#scores_table tr")
 const apiRoot = "http://localhost:8000"
 let active = "daily"
+let user_name = window.localStorage["user_name"] || ""
+let highscore = 0
+nameEntry.value = user_name
 
 function updateActiveTab(id){
     active = id
@@ -48,6 +52,17 @@ function populateScores(){
             leaderboardRows[i].querySelector(".score").textContent = score
         }
     })
+    if (user_name){
+        axios.get(apiRoot + filter, {params: {
+            name: user_name
+        }})
+        .then(res => {
+            console.log(res.data)
+            if(res.data.length > 0){
+                highscore = res.data[0].score
+            }
+        })
+    }
 }
 
 tabs.forEach(e => {
@@ -61,7 +76,7 @@ const submitButton = buttons[0];
 
 function enableScoreSubmitBox(score){
     submitButton.onclick = () => {
-        let name = document.getElementById("submit_name").value
+        let name = nameEntry.value
         submitScore(name, score)
         submitButton.style.enabled = false
     }
@@ -74,11 +89,14 @@ function disableScoreSubmitBox(){
 
 function submitScore(name, score){
     let data = new FormData()
+    user_name = name
+    localStorage["name"] = name
     data.append("name", name)
     data.append("score", score)
 
     axios.post(apiRoot + "/scores/submit", data)
     .then(res => {
+        console.log(res.data)
         disableScoreSubmitBox()
         populateScores()
     })
